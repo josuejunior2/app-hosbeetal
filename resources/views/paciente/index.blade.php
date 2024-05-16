5c6ed5aa98d47851a74ee7567df273e5efbb4d8f
josue <josuejuniorww@gmail.com>
2024-05-16 10:42:13 -0300

    fix: index paciente

--- resources/views/paciente/index.blade.php
+++ resources/views/paciente/index.blade.php
@@ -21,8 +21,9 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 15l6 -6l6 6" /></svg>
               </th> --}}
               <th>Nome</th>
-              <th>Telefone</th>
+              <th>CPF</th>
               <th>Email</th>
+              <th>Data de nascimento</th>
               <th></th>
             </tr>
           </thead>
@@ -32,8 +33,9 @@
                 <!--</td>  <input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"> -->
                 {{-- <td><span class="text-muted">{{ $paciente->id }}</span></td> --}}
                 <td>{{ $paciente->nome }}</td>
-                <td>{{ $paciente->telefone }}</td>
+                <td>{{ $paciente->cpf }}</td>
                 <td>{{ $paciente->email }}</td>
+                <td>{{ \Carbon\Carbon::parse($paciente->data_nascimento)->format('d/m/Y') }}</td>
                 <td class="text-end">
                     <a class="btn justify-content-center" href="{{ route('paciente.show', ['paciente' => $paciente]) }}">
                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-eye"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
